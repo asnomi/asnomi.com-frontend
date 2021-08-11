81b2a94bef9959dd668ef9c5b752eab4596f9e15
asnomi <asnomi.com@gmail.com>
2021-08-11 20:21:55 +0900

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# asnomi.com-frontend
-Portfolio website frontend repository
+# asnomi.com contents repos
+
+## Operation manual
+### Directorys and Files
+ディレクトリ構成とファイルは以下の配置となっている。
+
+```
+.
+├── docroot
+│   ├── contents/ # 雑多なページ
+│   ├── img/ # 画像置き場
+│   ├── style/ # css置き場
+│   ├── biography.html # biography
+│   ├── discography.html # discography
+│   └── index.html # index
+├── pug
+│   ├── include/ # 共通モジュール
+│   ├── biography.pug # biographyページ
+│   ├── discography.pug # discographyページ
+│   ├── index.pug # indexページ
+├── README.md # this file
+├── deploy.sh # deploy script
+└── env # 環境変数ファイル
+```
+
+### Create files and contents
+
++ pugファイルを作成もしくは編集して```pug/```以下に配置
++ cssファイルを作成もしくは編集して```docroot/style/```以下に配置
++ よしなに画像コンテンツを作成して```docroot/img/```以下に配置
+
+### Deployment
+以下スクリプトを実行
+
+```bash
+./deploy.sh
+```
+
++ 実行される処理
+  - S3のクリーンアップ
+  - Pugコンパイル
+  - S3にアップロード
+  - CloudFrontのキャッシュ削除
+
+S3のクリーンアップとキャッシュ削除は必要な場合にコメントアウトを外して有効化する。
+
+## Require Tools
++ AWS CLI
++ Pug(Jade)
---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+## include
+. ./env
+
+cd ${ROOTDIR}
+
+echo "===================================="
+echo "Deploy start: ${BUCKET}"
+echo "===================================="
+
+#echo "S3 Cleanup"
+#aws s3 rm s3://${BUCKET}/img/ ${OPTION}
+#aws s3 rm s3://${BUCKET}/contents/ ${OPTION}
+#aws s3 rm s3://${BUCKET}/style/ ${OPTION}
+#echo "Cleanup Finished"
+
+echo "Pug Compile"
+cd pug
+pug *.pug
+mv work*.html ../docroot/contents/   
+mv discography.html ../docroot/
+mv biography.html ../docroot/
+mv index.html ../docroot/
+
+echo "Compile Finished"
+
+echo "Upload to S3"
+cd ..
+aws s3 cp docroot/img/ s3://${BUCKET}/img/ ${OPTION}
+aws s3 cp docroot/contents/ s3://${BUCKET}/contents/ ${OPTION}
+aws s3 cp docroot/style/ s3://${BUCKET}/style/ ${OPTION}
+aws s3 cp docroot/index.html s3://${BUCKET}/
+aws s3 cp docroot/discography.html s3://${BUCKET}/
+aws s3 cp docroot/biography.html s3://${BUCKET}/
+
+echo "Uploading Finished"
+
+#echo "Cache Parge"
+#aws cloudfront create-invalidation --distribution-id ${DISTID} --paths "/*"
+
+echo "===================================="
+echo "done"
+echo "===================================="
