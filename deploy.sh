0499c4f45065a728c7ce425cc05a86d38b552b4d
asnomi <asnomi.com@gmail.com>
2021-08-14 21:36:39 +0900

    remove biography

--- deploy.sh
+++ deploy.sh
@@ -20,19 +20,17 @@ cd pug
 pug *.pug
 mv work*.html ../docroot/contents/   
 mv discography.html ../docroot/
-mv biography.html ../docroot/
 mv index.html ../docroot/
 
 echo "Compile Finished"
 
 echo "Upload to S3"
 cd ..
-aws s3 cp docroot/img/ s3://${BUCKET}/img/ ${OPTION}
+#aws s3 cp docroot/img/ s3://${BUCKET}/img/ ${OPTION}
 aws s3 cp docroot/contents/ s3://${BUCKET}/contents/ ${OPTION}
 aws s3 cp docroot/style/ s3://${BUCKET}/style/ ${OPTION}
 aws s3 cp docroot/index.html s3://${BUCKET}/
 aws s3 cp docroot/discography.html s3://${BUCKET}/
-aws s3 cp docroot/biography.html s3://${BUCKET}/
 
 echo "Uploading Finished"
 
